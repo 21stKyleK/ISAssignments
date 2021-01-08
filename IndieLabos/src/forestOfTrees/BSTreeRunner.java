7e1fb00ce8ef8dfdab4102d313bf68676342b047
21stKyleK <70537456+21stKyleK@users.noreply.github.com>
2021-01-07 20:47:01 -0600

    changed for loop

--- IndieLabos/src/forestOfTrees/BSTreeRunner.java
+++ IndieLabos/src/forestOfTrees/BSTreeRunner.java
@@ -13,11 +13,19 @@ public class BSTreeRunner
  		//add test cases here
  		
  		BinarySearchTree bruh = new BinarySearchTree();
+	   
+	   int[] testVals = {13, 54, 23, 1, 1, -34, 93, 17, 8, 7, -12, 43, 8, 43}
  		
+	   /*
  		for(int i = 0; i < 20; i+=2) {
  			bruh.add(i);
 			//all values in for loop go to right
  		}
+		*/
+	   
+	   for(int j : testVals){
+		bruh.add(j);   
+	   }
  		
  		bruh.inOrder();
  		bruh.preOrder();
