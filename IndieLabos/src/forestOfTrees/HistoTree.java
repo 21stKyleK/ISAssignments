c8f95473d69d37b8a6b8c2f2b0dc127f489a0f23
21stKyleK <70537456+21stKyleK@users.noreply.github.com>
2021-01-08 08:46:44 -0600

    did lab 3 for trees
    looked at solution, no test cases yet

--- IndieLabos/src/forestOfTrees/HistoTree.java
+++ IndieLabos/src/forestOfTrees/HistoTree.java
@@ -16,30 +16,63 @@ public class HistoTree
 
 	public void addData(Comparable data)
 	{
+		//root = add(data, root);
+		HistoNode gotten = search(data);
+		
+		if(gotten == null){
+			root = add(data, root);
+		}else{
+			found.setDataCount(found.getDataCount() + 1);
+		}
 	}
 
 	private HistoNode add(Comparable data, HistoNode tree)
 	{
-		return null;
+		if(tree == null){
+			return new HistoNode(data, 1, null, null);
+		}
+		int comparedV = data.compareTo(tree.getData());
+		
+// 		if(comparedV==0)
+// 			return tree.setDataCount(tree.getDataCount() + 1);
+		else if(comparedV<0)
+// 			return add(data, tree.getLeft());
+			return tree.setLeft(add(data, tree.getLeft()));
+		else
+// 			return add(data, tree.getRight());
+			return tree.setRight(add(data, tree.getRight()));
 	}
 
 	public HistoNode search(Comparable data)
 	{
-		return null;
+		return search(data, root);
 	}
 
 	private HistoNode search(Comparable data, HistoNode tree)
 	{
+		if(tree != null){
+			int comparedV = data.compareTo(tree.getData());
+			//if negative, then the first value is lower than the parameter
+			
+			if(comparedV==0)
+				return tree;
+			else if(comparedV<0)
+				return search(tree.getLeft());
+			else
+				return search(tree.getRight());
+		}
 		return null;
 	}
 
 	public String toString()
 	{
-		return "";
+		return toString(root).trim();
 	}
 
 	private String toString(HistoNode tree)
 	{
-		return "";
+		if(tree == null)
+			return "";
+		return toString(tree.getLeft()) + "  " + tree.getData() + "--" + tree.getDataCount() + "  " + toString(tree.getRight());
 	}
 }
