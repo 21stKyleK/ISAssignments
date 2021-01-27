ad01239736566377d4726db706b171facd0c268f
21stKyleK <70537456+21stKyleK@users.noreply.github.com>
2021-01-26 22:52:59 -0600

    filled in lab 3
    used solution for toString and to rewrite because I didn't catch the usage of TreeSets in the TreeMap. Also to fix the check code?

--- IndieLabos/src/graphinate/BiDirectionalGraph.java
+++ IndieLabos/src/graphinate/BiDirectionalGraph.java
@@ -20,19 +20,48 @@ public class BiDirectionalGraph
 
 	public BiDirectionalGraph(String line)
 	{
+		map = new TreeMap<String, TreeSet<String> >();
+		found = false;
+		String[] bruh = line.split[" "];
+		
+		for(int i = 0; i<bruh.length; i += 2){
+			String a = bruh[i], b = bruh[i+1];
+			
+			if(map.get(a) == null){
+				map.put(a, new TreeSet<String>());
+			}if(map.get(b) == null){
+				map.put(b, new TreeSet<String>());
+			}
+			
+			map.put(a, map.get(a).add(b));
+			map.put(b, map.get(b).add(a));
+		}
 	}
 
 	public boolean contains(String name)
 	{
-		return true;
+		boolean arg = (map.get(name) == null) ? false : true;
+		return arg;
 	}
 
 	public void check(String first, String second, TreeSet<String> placedUsed)
 	{
+		if(first.equals(second)){
+			found = true;
+		}else{
+			for(String j : map.get(a)){
+				if(placedUsed.get(j) == null){
+					placedUsed.add(j)
+					check(j, second, placedUsed);
+					placedUsed.remove(j)
+				}
+			}
+		}
 	}
 
 	public String toString()
 	{
-		return "";
+		String brarg = (found)? "Yee" : "No";
+		return brarg;
 	}
 }
