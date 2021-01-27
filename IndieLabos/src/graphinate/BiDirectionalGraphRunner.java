3457cae1923a7e908845b93df5174a88b29c21f6
21stKyleK <70537456+21stKyleK@users.noreply.github.com>
2021-01-26 22:59:36 -0600

    filled in lab 3 runner
    used solution to fill in a line

--- IndieLabos/src/graphinate/BiDirectionalGraphRunner.java
+++ IndieLabos/src/graphinate/BiDirectionalGraphRunner.java
@@ -14,5 +14,19 @@ public class BiDirectionalGraphRunner
 {
 	public static void main( String[] args ) throws IOException
 	{
+		Scanner arg = new Scanner(new File("./bidgraph.dat"));
+		
+		int cases = arg.nextInt();
+		
+		while(cases --> 0){
+			BiDirectionalGraph yee = new BiDirectionalGraph(arg.nextLine());
+			
+			String[] bef = arg.nextLine().split[" "];
+			
+			if(yee.contains(bef[0]) && yee.contains(bef[1]))
+				yee.check(bef[0], bef[1], new TreeSet<String>());
+			
+			System.out.println(bef[0] + " IS CONNECTED TO " + bef[1] + " : " + yee);
+		}
 	}
 }
