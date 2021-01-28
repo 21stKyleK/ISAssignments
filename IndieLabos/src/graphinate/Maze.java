b1ed634f1d31ce63ac844718e247c6b9d65522e8
21stKyleK <70537456+21stKyleK@users.noreply.github.com>
2021-01-27 19:22:17 -0600

    filled in lab 4
    used solution to fill in toString

--- IndieLabos/src/graphinate/Maze.java
+++ IndieLabos/src/graphinate/Maze.java
@@ -13,20 +13,49 @@ public class Maze
 
 	public Maze()
 	{
+		maze = {
+			{1, 0, 0, 0, 0},
+			{1, 1, 1, 0, 1},
+			{1, 0, 1, 1, 1},
+			{1, 1, 1, 0, 0},
+			{0, 1, 0, 0, 0}
+		};
 	}
 
 	public Maze(int[][] m)
 	{
+		maze = m;
 	}
 
 	public boolean checkForExitPath(int r, int c)
 	{
-		return false;
+		boolean bruh = false;
+		if(r >= 0 && r < maze.length && c >= 0 && c < maze[r].length && maze[r][c] == 1){
+			maze[r][c] = 2;
+			if(c >= maze[r].length - 1){
+				//bruh = true;
+				return true;
+			}else{
+				bruh = (checkForExitPath(r, c + 1) || checkForExitPath(r + 1, c) || checkForExitPath(r, c - 1) || checkForExitPath(r - 1, c));
+			}
+		}
+		return bruh;
 	}
 
 	public String toString()
 	{
+// 		String output= (checkForExitPath(0,0)) ? "exit found": "no escape";
+// 		return output;
+		
 		String output="";
+		for(int r= 0; r<maze.length; r++)
+		{
+			for(int c =0; c<maze[r].length; c++)
+			{
+				output+=maze[r][c]+" ";
+			}
+			output+="\n";
+		}
 		return output;
 	}
 }
